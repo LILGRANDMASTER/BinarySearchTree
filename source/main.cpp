4eb437acc6e2c0cdc912fb3f7b7c113e1e7f7e38
LILGRANDMASTER <morojenka111@gmail.com>
2022-01-06 01:10:44 +0300

    menu() added

--- source/main.cpp
+++ source/main.cpp
@@ -1,24 +1,57 @@
 #include <iostream>
 #include "BinTree.h"
-#include <stdio.h>
 
+void menu(){
+    std::cout<<"1.             ADD NODE"<<std::endl;
+    std::cout<<"2.          DELETE NODE"<<std::endl;
+    std::cout<<"3.    PRINT PREFIX TREE"<<std::endl;
+    std::cout<<"4.   PRINT POSTFIX TREE"<<std::endl;
+    std::cout<<"5.     PRINT INFIX TREE"<<std::endl;
+    std::cout<<"6.          END PROGRAM"<<std::endl;
+}
 
 int main(){
 
 	BinTree<int> tree;
-	tree.insert(1);
-	tree.insert(10);
-	tree.insert(-10);
-	tree.insert(5);
-	tree.insert(6);
-	tree.insert(3);
-	tree.insert(20);
-	tree.insert(-20);
-	tree.insert(-4);
-	tree.print(3);
-	tree.deleteNode(5);
-	std::cout<<"================================"<<std::endl;
-	tree.print(3);
+    
+    while(true){
+        
+        menu();
+        int comm;
+        std::cout<<"Command: ";
+        std::cin>>comm;
+        int x = 0;
+
+        switch(comm){
+
+            case 1:     std::cout<<"Enter node you want to add: ";
+                        std::cin>>x;
+                        tree.insert(x);
+                        break;
+
+            case 2:     std::cout<<"Enter node you want to delete: ";
+                        std::cin>>x;
+                        tree.deleteNode(x);
+                        break;
+
+            case 3:     tree.print(1);
+                        break;
+
+            case 4:     tree.print(2);
+                        break;
+
+            case 5:     tree.print(3);
+                        break;
+
+            case 6:     return 0;
+                        break;
+
+            default:    std::cout<<"Unknown command!!!"<<std::endl;
+                        break;
+
+
+        }
+    }
 	
 	return 0;
 }
